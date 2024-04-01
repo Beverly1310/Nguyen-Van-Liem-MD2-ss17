6dfb124719cf5358ce5e7d012d9fc4227da0d135
Beverly1310 <nliem468@gmail.com>
2024-04-01 16:01:44 +0700

    update

--- src/bai11/Bai11.java
+++ src/bai11/Bai11.java
@@ -8,34 +8,40 @@ public class Bai11 {
         checkDate(scanner);
 
     }
+
     public static void checkDate(Scanner scanner) throws DateException {
 
         System.out.println("Nhap vao nam: ");
         int year = Integer.parseInt(scanner.nextLine());
-        if (year<0){
+        if (year < 0) {
             throw new DateException("Nam khong hop le");
         }
         System.out.println("Nhap vao thang: ");
         int month = Integer.parseInt(scanner.nextLine());
-        if (month<1 || month>12){
+        if (month < 1 || month > 12) {
             throw new DateException("Thang khong hop le");
         }
         System.out.println("Nhap vao ngay: ");
         int date = Integer.parseInt(scanner.nextLine());
-        switch (month){
-            case 1,3,5,7,8,10,12:
-                if (date<1 || date>31) {
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                if (date < 1 || date > 31) {
                     throw new DateException("Ngay khong hop le");
                 }
                 break;
-            case 4,6,9,11:
-                if (date<1 || date>30) {
+            case 4, 6, 9, 11:
+                if (date < 1 || date > 30) {
                     throw new DateException("Ngay khong hop le");
                 }
                 break;
             case 2:
-                if (date<1 || date>29) {
-                    throw new DateException("Ngay khong hop le");
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    if (date < 1 || date > 29)
+                        throw new DateException("Ngay khong hop le");
+                }
+                else {
+                    if (date < 1 || date > 28)
+                        throw new DateException("Ngay khong hop le");
                 }
                 break;
         }
